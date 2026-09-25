62b5f2117d11fa0d00f58c4374f4516055264250
Bhargav Elindram <bhargavelindram@gmail.com>
2026-09-26 00:57:40 +0530

    Switch vision agent to faster 3B vision model

--- accident_ai/server.py
+++ accident_ai/server.py
@@ -22,7 +22,7 @@ async def add_local_network_access_header(request, call_next):
     response.headers["Access-Control-Allow-Private-Network"] = "true"
     return response
 OLLAMA_URL = os.getenv("OLLAMA_URL", "http://127.0.0.1:11434").rstrip("/")
-VISION_MODEL = os.getenv("OLLAMA_VISION_MODEL", "llava:7b")
+VISION_MODEL = os.getenv("OLLAMA_VISION_MODEL", "qwen2.5vl:3b")
 FRAME_TIMEOUT = max(90.0, float(os.getenv("VISION_TIMEOUT", "90")))
 ALERT_COOLDOWN = float(os.getenv("VISION_ALERT_COOLDOWN", "2"))
 last_alert_by_camera: Dict[str, float] = {}
